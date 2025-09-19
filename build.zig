8362d1bf7fee1dce7d541333380d3d9a7495d96b
ousttrue <ousttrue@gmail.com>
2025-09-20 02:20:22 +0900

    remove build.zig

--- build.zig
+++
@@ -1,7 +0,0 @@
-const std = @import("std");
-
-pub fn build(b: *std.Build) void {
-    const wf = b.addNamedWriteFiles("glTF-Sample-Assets");
-    _ = wf.addCopyDirectory(b.path("glTF-Sample-Assets/Models/"), "Models", .{});
-    b.default_step.dependOn(&wf.step);
-}
